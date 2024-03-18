52c7ec61097183059d99f1464417fafc08d1dbc9
DainiusBal <balionisdainius@gmail.com>
2024-03-18 11:33:38 +0200

    Initial commit

---
+++ dainius-sp-service/src/main/java/com/balionis/dainius/sps/DainiusStocksPricingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.balionis.dainius.sps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainiusStocksPricingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DainiusStocksPricingServiceApplication.class, args);
+	}
+
+}
