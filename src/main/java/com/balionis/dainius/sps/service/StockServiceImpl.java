fbb651e2c9071f673cb405f92d8cb1b0a7ddb9f9
DainiusBal <balionisdainius@gmail.com>
2024-04-05 13:44:53 +0300

    Configure liquibase with Version: 4.22.0

--- src/main/java/com/balionis/dainius/sps/service/StockServiceImpl.java
+++ src/main/java/com/balionis/dainius/sps/service/StockServiceImpl.java
@@ -91,8 +91,6 @@ public class StockServiceImpl implements StockService {
         }
     }
 
-
-
     @Override
     public List<Stock> findStocksByTicker(String ticker) {
         return stockRepository.findByTickerContaining(ticker);
