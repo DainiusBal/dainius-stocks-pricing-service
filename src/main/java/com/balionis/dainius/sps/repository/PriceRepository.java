959a00ee9b8d39a4556de4f6dabe4478c93633d9
Arvydas Balionis <arvydas@balionis.com>
2024-04-20 07:12:53 +0100

    DBAL-0004 rename Stock.id to Stock.stockId

--- src/main/java/com/balionis/dainius/sps/repository/PriceRepository.java
+++ src/main/java/com/balionis/dainius/sps/repository/PriceRepository.java
@@ -2,14 +2,15 @@ package com.balionis.dainius.sps.repository;
 
 import com.balionis.dainius.sps.model.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface PriceRepository extends JpaRepository<Price, Long> {
+    @Query("select p from Price p where p.stock.stockId = ?1 AND p.pricingDate >= ?2 AND p.pricingDate <= ?3")
     List<Price> findByStockIdAndPricingDateBetween(String stockId, LocalDate fromDate, LocalDate toDate);
 }
 
